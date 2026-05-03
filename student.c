a58b0de038d152124f3d5867d2a3a87bdcc6e96e
bentejisaac-arch <bentejisaac@gmail.com>
2026-05-03 11:04:22 +0200

    Update student.c

--- student.c
+++ student.c
@@ -1,1 +1,12 @@
+#include "prototypes.h"
 
+// Fonction principale du mode étudiant
+void run_student_mode() {
+    
+    printf("\n=== STUDENT MODE ===\n");
+    printf("This section is under construction...\n");
+    
+    // La Personne 2 écrira toute sa logique ici plus tard
+    // (affichage des questions, calcul du score, etc.)
+}
+#coquile vide histoire de pouvoir faire un test a modifier juste apres
