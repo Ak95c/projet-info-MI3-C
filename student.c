0285fd7094bd7cd26b7ed774b2dde34c0bb121b0
bentejisaac-arch <bentejisaac@gmail.com>
2026-05-12 17:53:50 +0200

    Update student.c

--- student.c
+++ student.c
@@ -11,3 +11,40 @@ void run_student_mode() {
     //coquile vide histoire de pouvoir faire un test a modifier juste apres
     
 }
+#include "prototypes.h"
+
+void run_student_mode() {
+    Quiz q = load_quiz_from_file(); // On charge le quiz que le prof a créé
+    int score = 0;
+    int user_answer;
+
+    printf("\n--- BIENVENUE DANS LE QUIZ: %s ---\n", q.title);
+
+    for (int i = 0; i < q.nb_questions; i++) {
+        printf("\nQuestion %d: %s\n", i + 1, q.list[i].text);
+        for (int j = 0; j < 4; j++) {
+            printf("%d. %s\n", j + 1, q.list[i].options[j]);
+        }
+
+        printf("votre réponse (1-4): ");
+        scanf("%d", &user_answer);
+
+        if (user_answer == q.list[i].correct_option) {
+            printf("Correct!\n");
+            score++; // On gagne 1 point
+        } else {
+            printf("FAUX La bonne réponse était: %d.\n", q.list[i].correct_option);
+            // Si le prof a activé les points négatifs, on retire 1 point
+            if (q.negative_points == 1) {
+                score--;
+            }
+        }
+
+        // Si le mode séquentiel est activé, on prévient l'élève qu'il ne peut pas revenir en arrière
+        if (q.sequential_mode == 1) {
+            printf("[System] Sequential mode active: Answer locked.\n");
+        }
+    }
+
+    printf("\n--- SCORE FINAL: %d/%d ---\n", score, q.nb_questions);
+}
