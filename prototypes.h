7d44bc9ccd4b42aa268f2a93357af8a0c452fbcd
bentejisaac-arch <bentejisaac@gmail.com>
2026-05-19 15:28:50 +0200

    Update prototypes.h

--- prototypes.h
+++ prototypes.h
@@ -30,5 +30,5 @@ typedef struct {
 void run_teacher_mode();
 void run_student_mode();
 void save_quiz_to_file(Quiz q);
-
+Quiz load_quiz_from_file();//Elle ne prend aucun paramètre entre ses parenthèses, et quand elle a fini de travailler, elle renvoie une structure complète de type Quiz.
 #endif
